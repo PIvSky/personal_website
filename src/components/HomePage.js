4bba0ffb3ed36fc1799ed2dcadd47410f37fbae6
PIvSky <piotr.iwansky@gmail.com>
2023-11-03 20:18:17 +0100

    1. ImageSlider with RWD completed!

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,35 @@
+import ImageSlider from './ImageSlider';
 import './HomePage.scss';
+import photo1 from '../assets/img/photo-1.jpg';
+import photo2 from '../assets/img/photo-2.jpg';
+import photo3 from '../assets/img/photo-3.jpg';
+import photo4 from '../assets/img/photo-4.jpg';
+import photo5 from '../assets/img/photo-5.jpg';
+import photo6 from '../assets/img/photo-6.jpg';
+import photo7 from '../assets/img/photo-7.jpg';
 
 const HomePage = () => {
 
+    const photos = [
+        {url: photo1, title: 'photo-1'},
+        {url: photo2, title: 'photo-2'},
+        {url: photo3, title: 'photo-3'},
+        {url: photo4, title: 'photo-4'},
+        {url: photo5, title: 'photo-5'},
+        {url: photo6, title: 'photo-6'},
+        {url: photo7, title: 'photo-7'},
+
+    ]
+
     return (
         <>
-            <div className='container'>
-                <h1>Home Page</h1>
+            {/* <div className='home-container'>
+                <h1>HOME</h1>
+            </div> */}
+            <div className='home-container'>
+                <div className='slides-wrapper'>
+                    <ImageSlider slides={photos}></ImageSlider>
+                </div>
             </div>
         </>
     )
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,63 @@
+import {useState} from 'react';
+import './ImageSlider.scss';
+
+const ImageSlider = ({slides}) => {
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    //  ARROW FUNCTIONALITIES
+    const prevSlide = () => {
+        if (currentIndex > 0){
+            setCurrentIndex(currentIndex - 1)
+        } else {
+            setCurrentIndex(slides.length - 1)
+        }
+    }
+
+    const nextSlide = () => {
+        if (currentIndex < slides.length - 1) {
+        setCurrentIndex(currentIndex + 1)
+        } else {
+            setCurrentIndex(0)
+        }
+    }   
+
+    // DOTS FUNCTIONALITIES
+    const currentSlide = (index) => {
+        setCurrentIndex(index)
+    }
+
+    // SLIDE SHOW
+    setTimeout(() => {
+        if (currentIndex < slides.length - 1) {
+            setCurrentIndex(currentIndex + 1)
+            } else {
+                setCurrentIndex(0)
+            }
+    }, 5700)
+
+    return (
+        <div className='slides-container'>
+            <div className='left-arrow' onClick={prevSlide}>&#129168;</div>
+            <div 
+                style={{backgroundImage: `url(${slides[currentIndex].url})`,}}
+                className='slides-container__item'
+            >
+                <div className='dots-container'>
+                {slides.map((slides ,index) => (
+                    <div className='dots-container__items' key={index} onClick={() => currentSlide(index)}>&#183;</div>
+                ))}
+            </div>
+            </div>
+            <div className='right-arrow' onClick={nextSlide}>&#129170;</div>
+            {/* <div className='dots-container'>
+                {slides.map((slides ,index) => (
+                    <div className='dots-container__items' key={index} onClick={() => currentSlide(index)}>&#183;</div>
+                ))}
+            </div> */}
+        </div>
+    )
+
+}
+
+export default ImageSlider;

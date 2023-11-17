864c0863071a826b4cae3902cf85cbfd85877521
PIvSky <piotr.iwansky@gmail.com>
2023-11-17 01:22:00 +0100

    Completed Works.js component

--- src/components/Composer.js
+++ src/components/Composer.js
@@ -1,7 +1,37 @@
-const Composer = () => {
+import WorksTemplate from "./WorksTemplate";
+
+const data = [
+    {
+        title: "GCH+ - ŚWINA",
+        id: Math.random(),
+        content: "Composed for 'Timelapse'. This composition is inspired by the river flowing through Świnoujście.",
+        src: 'https://www.youtube.com/embed/7CHfIw1VXBo?si=eun4KCvmzrA3U8e-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+    },
+    {
+        title: "GCH+ - HIGHWAY",
+        id: Math.random(),
+        content: "Composed for 'Timelapse'. This composition is a result of combination of hip-hop and jazz music.",
+        src: 'https://www.youtube.com/embed/BsostpRvY1Q?si=eRw8ZTcuTaasnUwi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+    },
+    {
+        title: "GCH+ - S3",
+        id: Math.random(),
+        content: "Also composed for 'Timelapse'. S3 is a name of road in West Poland. This composition is about beeing on tour as a common part of musician life.",
+        src: 'https://www.youtube.com/embed/FqSJ0MV2W3c?si=I9s-3eM8-VPSR2Ge" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+    }
+]
+
+const Composer  = () => {
     return (
         <>
-            <p>Smth</p>
+            {data.map((data) => (
+                <WorksTemplate
+                key={data.id}
+                title={data.title}
+                content={data.content}
+                src={data.src}
+            />
+            ))}
         </>
     )
 };

8674ed5a7614e872b6c6495cdd3c250fa543dd46
PIvSky <piotr.iwansky@gmail.com>
2024-03-23 23:09:34 +0100

    Changed Arranger component for multilingual

--- src/components/Arranger.js
+++ src/components/Arranger.js
@@ -1,33 +1,41 @@
+import { useTranslation } from "react-i18next";
 import WorksTemplate from "./WorksTemplate";
 
-const data = [
-    {
-        title: "RESERVED",
-        id: Math.random(),
-        content: "Arrangement and play on the piano song 'Yes Sir, I Can Boogie' by Baccara. You can also watch me on this movie.",
-        src: 'https://www.youtube.com/embed/OuMAGX6es7I?si=JVInVY9Y4LpCEgD6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
-    },
-    {
-        title: "POZYTYWKA - Czesław Mozil & Grajkowie Przyszłości",
-        id: Math.random(),
-        content: "Arrangment for symphony orchestra a song of popular Polish Band Farben Lehre.",
-        src: 'https://www.youtube.com/embed/4Va-JIez21E?si=M6h_SxkW84KnctXG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
-    },
-    {
-        title: "Grzeczni Chłopcy: Swigin' Boys- Moves Like Jagger (Maroon 5 Cover)",
-        id: Math.random(),
-        content: "Swingin arrangement of popular Marron 5 song for jazz quintet.",
-        src: 'https://www.youtube.com/embed/aVFOHu9FRfw?si=n0HRasvarrXN8r7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
-    },
-    {
-        title: "Grzeczni Chłopcy - Human Nature",
-        id: Math.random(),
-        content: "Arrangment of Michael Jackson's song for jazz quintet. ",
-        src: 'https://www.youtube.com/embed/oQsftb7ppCA?si=l3zFE2wlEmv_fuSX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-    }
-]
-
 const Arranger = () => {
+
+    const [t] = useTranslation("global");
+    const reserved = t("arranger.reserved");
+    const mozil = t("arranger.mozil");
+    const maroon = t("arranger.maroon");
+    const jackson = t("arranger.jackson")
+
+    const data = [
+        {
+            title: "RESERVED",
+            id: Math.random(),
+            content: reserved,
+            src: 'https://www.youtube.com/embed/OuMAGX6es7I?si=JVInVY9Y4LpCEgD6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
+        },
+        {
+            title: "POZYTYWKA - Czesław Mozil & Grajkowie Przyszłości",
+            id: Math.random(),
+            content: mozil,
+            src: 'https://www.youtube.com/embed/4Va-JIez21E?si=M6h_SxkW84KnctXG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
+        },
+        {
+            title: "Grzeczni Chłopcy: Swigin' Boys- Moves Like Jagger (Maroon 5 Cover)",
+            id: Math.random(),
+            content: maroon,
+            src: 'https://www.youtube.com/embed/aVFOHu9FRfw?si=n0HRasvarrXN8r7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"'
+        },
+        {
+            title: "Grzeczni Chłopcy - Human Nature",
+            id: Math.random(),
+            content: jackson,
+            src: 'https://www.youtube.com/embed/oQsftb7ppCA?si=l3zFE2wlEmv_fuSX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        }
+    ]
+
     return (
         <>
             {data.map((data) => (

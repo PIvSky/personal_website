a4924412b76f01c8e98a13c334efbd32899afb34
PIvSky <piotr.iwansky@gmail.com>
2024-03-25 14:25:23 +0100

    Changed Composer component for multilinguage

--- src/components/Composer.js
+++ src/components/Composer.js
@@ -1,27 +1,35 @@
+import { useTranslation } from "react-i18next";
 import WorksTemplate from "./WorksTemplate";
 
-const data = [
-    {
-        title: "GCH+ - ŚWINA",
-        id: Math.random(),
-        content: "Composed for 'Timelapse'. This composition is inspired by the river flowing through Świnoujście.",
-        src: 'https://www.youtube.com/embed/7CHfIw1VXBo?si=eun4KCvmzrA3U8e-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-    },
-    {
-        title: "GCH+ - HIGHWAY",
-        id: Math.random(),
-        content: "Composed for 'Timelapse'. This composition is a result of combination of hip-hop and jazz music.",
-        src: 'https://www.youtube.com/embed/BsostpRvY1Q?si=eRw8ZTcuTaasnUwi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-    },
-    {
-        title: "GCH+ - S3",
-        id: Math.random(),
-        content: "Also composed for 'Timelapse'. S3 is a name of road in West Poland. This composition is about beeing on tour as a common part of musician life.",
-        src: 'https://www.youtube.com/embed/FqSJ0MV2W3c?si=I9s-3eM8-VPSR2Ge" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-    }
-]
-
 const Composer  = () => {
+
+    const [t] = useTranslation("global");
+
+    const swinaContent = t("composer.swina");
+    const highwayContent = t("composer.highway");
+    const s3Content = t("composer.s3")
+
+    const data = [
+        {
+            title: "GCH+ - ŚWINA",
+            id: Math.random(),
+            content: swinaContent,
+            src: 'https://www.youtube.com/embed/7CHfIw1VXBo?si=eun4KCvmzrA3U8e-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        },
+        {
+            title: "GCH+ - HIGHWAY",
+            id: Math.random(),
+            content: highwayContent,
+            src: 'https://www.youtube.com/embed/BsostpRvY1Q?si=eRw8ZTcuTaasnUwi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        },
+        {
+            title: "GCH+ - S3",
+            id: Math.random(),
+            content: s3Content,
+            src: 'https://www.youtube.com/embed/FqSJ0MV2W3c?si=I9s-3eM8-VPSR2Ge" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        }
+    ];
+
     return (
         <>
             {data.map((data) => (
